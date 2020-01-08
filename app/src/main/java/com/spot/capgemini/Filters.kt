48dff93469ed0c66b72a513d82a244642fba0759
abdul <abdul.tanveer@gmail.com>
2020-01-08 16:27:38 +0530

    higher order functions

--- app/src/main/java/com/spot/capgemini/Filters.kt
+++ app/src/main/java/com/spot/capgemini/Filters.kt
@@ -18,10 +18,14 @@ fun main() {
 
 
     var dirtyLevel = 20
-    val waterFilter = { dirty : Int -> dirty / 2}
-    println(waterFilter(dirtyLevel))
+    val waterFilter: (Int) -> Int = { dirty -> dirty / 2 }
+    println(updateDirty(30, waterFilter))
 }
 
 /*private  int getWaterFilter(int dirty){
     return dirty/2;
 }*/
+
+fun updateDirty(dirty: Int, operation: (Int) -> Int): Int {
+    return operation(dirty)
+}
