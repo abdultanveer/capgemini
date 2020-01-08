154c73e7046caecbc9578c0bf877b1d5dbaed04d
abdul <abdul.tanveer@gmail.com>
2020-01-08 15:42:39 +0530

    filters map lazy

--- app/src/main/java/com/spot/capgemini/Filters.kt
+++ app/src/main/java/com/spot/capgemini/Filters.kt
@@ -3,5 +3,11 @@ package com.spot.capgemini
 val decorations = listOf ("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
 
 fun main() {
-    println( decorations.filter {it[0] == 'p'})
+    //println( decorations.filter {it[0] == 'p'})
+    /*val filtered = decorations.asSequence().filter { it[0] == 'p' }
+    println("filtered: " + filtered)*/
+    val lazyMap = decorations.asSequence().map {
+        println("access: $it")
+        it
+    }
 }
