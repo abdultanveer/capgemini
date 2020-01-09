6b6b44bf446f52605cca084ff70363d5ef75963f
abdul <abdul.tanveer@gmail.com>
2020-01-09 12:16:51 +0530

    data class

--- app/src/main/java/com/spot/capgemini/classes/main.kt
+++ app/src/main/java/com/spot/capgemini/classes/main.kt
@@ -3,6 +3,16 @@ package com.spot.capgemini.classes
 fun main() {
     /*val dog: Dog = Yorkshire()
     dog.sayHello()*/
-    val tiger: Tiger = SiberianTiger()
-    tiger.sayHello()
+    /*val tiger: Tiger = SiberianTiger()
+    tiger.sayHello()*/
+    val decoration2 = Decoration2("rocks","wood","diver")
+    //decoration2.makeDecorations()
+    val d5 = Decoration2("crystal", "wood", "diver")
+    println(d5)
+
+// Assign all properties to variables.
+    val (rock, diver) = d5
+    println(rock)
+   // println(wood)
+    println(diver)
 }
